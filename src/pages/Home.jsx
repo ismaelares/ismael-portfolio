d0d22f3c3e8b1e7f42b584d35ff5e63a8f9a0d20
Ismael Lares <ismael.lardom@gmail.com>
2025-12-19 09:27:54 -0800

    add navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
       <StarBackground />
-      Home Page
+      <Navbar />
     </div>
   );
 };
